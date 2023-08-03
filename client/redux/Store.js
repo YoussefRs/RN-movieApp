0c359dc12fd3209c6695fdc28437715e5536ffe8
YoussefRs <rs.youssef24@gmail.com>
2023-08-03 17:17:18 +0100

    init'

--- client/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import MovieReducer from "./MovieSlice"
-
-export const store = configureStore({
-    reducer: {
-        Movie : MovieReducer
-    }
-})
